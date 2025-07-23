f49483e71d6d4228a8a7b2d4d12ebc622f7182aa
sohila558 <sohailat.441@gmail.com>
2025-07-23 21:07:13 +0300

    Inheritance

--- AssignmentOOP2/Program.cs
+++ AssignmentOOP2/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace AssignmentOOP2
+﻿using AssignmentOOP2.Inheritance;
+
+namespace AssignmentOOP2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Inheritance
+            // 2. Inheritance : 
+            // X --> Y
+            // DRY
+
+
+            //Parent parent = new Parent(1, 2);
+
+            // Console.WriteLine(parent);
+
+            //parent.Fun01();
+            //parent.Fun02();
+
+            //Child child = new Child(1, 2, 3);
+
+            //child.Fun01();
+            //child.Fun02(); 
+            #endregion
+
+
+
         }
     }
 }
